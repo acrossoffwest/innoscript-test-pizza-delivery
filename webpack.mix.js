dfbce2897f8a0addd85e508e4d3b28222f284051
Yurij Karpov <acrossoffwest@gmail.com>
2020-09-16 09:55:30 +0200

    (fix) Optimized image resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.browserSync({
+    proxy: 'http://innascripta-test-pizza.localhost'
+});
 
 mix
   .js('resources/js/app.js', 'public/js')
